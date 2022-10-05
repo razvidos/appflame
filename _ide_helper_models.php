bd9ff27f4ca923d6830f7ea2b51c985384dd6e23
ingvar-grodas <108241165+ingvar-grodas@users.noreply.github.com>
2022-10-06 02:06:53 +0300

    helper libs

---
+++ _ide_helper_models.php
@@ -0,0 +1,108 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Author
+ *
+ * @property int $author_id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Book[] $book
+ * @property-read int|null $book_count
+ * @method static \Database\Factories\AuthorFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Author newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Author query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereAuthorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereUpdatedAt($value)
+ */
+	class Author extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Book
+ *
+ * @property int $book_id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Author[] $author
+ * @property-read int|null $author_count
+ * @method static \Database\Factories\BookFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
+ */
+	class Book extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\BookAuthor
+ *
+ * @property int $author_id
+ * @property int $book_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\BookAuthorFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor whereAuthorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BookAuthor whereUpdatedAt($value)
+ */
+	class BookAuthor extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
